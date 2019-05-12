4b9ac31b0fc2d201d97c4da97e558c3e91276c51
Juan Pa <jpabloascencioglez@gmail.com>
2019-05-11 19:27:39 -0500

    django_heroku

--- ticketnoobster/ticketnoobster/settings.py
+++ ticketnoobster/ticketnoobster/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -124,6 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+django_heroku.settings(locals())
 
 # CORS_ORIGIN_ALLOW_ALL = True
 # CORS_ALLOW_CREDENTIALS = True
