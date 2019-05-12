3d4645fc68d31167bf97ebd611266b4a8a4d2ea2
Juan Pa <jpabloascencioglez@gmail.com>
2019-05-11 19:03:50 -0500

    removed cors

--- ticketnoobster/ticketnoobster/settings.py
+++ ticketnoobster/ticketnoobster/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'corsheaders',
+    # 'corsheaders',
     'store'
 ]
 
@@ -50,8 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',  
-    'django.middleware.common.CommonMiddleware',
+    # 'corsheaders.middleware.CorsMiddleware',  
+    # 'django.middleware.common.CommonMiddleware',
 ]
 
 
@@ -127,4 +127,4 @@ STATIC_URL = '/static/'
 
 # CORS_ORIGIN_ALLOW_ALL = True
 # CORS_ALLOW_CREDENTIALS = True
-CORS_ORIGIN_WHITELIST = 'localhost:8080', 
+# CORS_ORIGIN_WHITELIST = 'localhost:8080', 
